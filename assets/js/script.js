c4664be555a76589aebcbea4766755507f9f39a7
yogaardiansyah <yogaardiansyah@protonmail.com>
2025-03-12 05:41:05 +0700

    feat: Projects, contact and fix CSS Issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,25 @@
 document.addEventListener("scroll", function () {
-    let sections = document.querySelectorAll("section");
-    let links = document.querySelectorAll("nav ul li a");
+  let sections = document.querySelectorAll("section");
+  let links = document.querySelectorAll("nav ul li a");
 
-    sections.forEach(section => {
-        let top = window.scrollY;
-        let offset = section.offsetTop - 100;
-        let height = section.offsetHeight;
-        let id = section.getAttribute("id");
+  sections.forEach((section) => {
+    let top = window.scrollY;
+    let offset = section.offsetTop - 100;
+    let height = section.offsetHeight;
+    let id = section.getAttribute("id");
 
-        if (top >= offset && top < offset + height) {
-            links.forEach(link => {
-                link.classList.remove("active");
-                if (link.getAttribute("href").includes(id)) {
-                    link.classList.add("active");
-                }
-            });
+    if (top >= offset && top < offset + height) {
+      links.forEach((link) => {
+        link.classList.remove("active");
+        if (link.getAttribute("href").includes(id)) {
+          link.classList.add("active");
         }
-    });
+      });
+    }
+  });
 });
 
-window.addEventListener('load', function () {
-    document.getElementById("homeLink").classList.add("active");
+window.addEventListener("load", function () {
+  document.getElementById("homeLink").classList.add("active");
 });
+
